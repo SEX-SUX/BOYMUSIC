25ea602e81fe9d0b9a563ed98664cd75249f0f92
Soulaizen77 <hritikraj123412312@gmail.com>
2024-11-24 17:18:10 +0530

    Update gpdata.py

--- Devine/programs/admins/gpdata.py
+++ Devine/programs/admins/gpdata.py
@@ -1,10 +1,10 @@
 from pyrogram import enums
 from pyrogram.enums import ChatType
 from pyrogram import filters, Client
-from AnonXMusic import app
+from Dev import app
 from config import OWNER_ID
 from pyrogram.types import Message
-from AnonXMusic.utils.jarvis_ban import admin_filter
+from Devine.utils.Dev_ban import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 
